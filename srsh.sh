c21ab41e5e4db42b7176ce522e3eb8f564001a96
Jordan <Jordancattwood@hotmail.com>
2019-10-15 20:53:51 +1100

    v1.7
    -updated srsg.sh to inorperate the bugfix for variable uname length
    -other checks/fixes in regards to getting the url

--- srsh.sh
+++ srsh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+#ssh://port31%3Bpassword%3DfDjg9BjZne@ictencsvr1.ict.swin.edu.au/
 
 #REQUIREMENTS:
 #Python 3.x
@@ -13,7 +14,9 @@ def url_host(str_input):
 
     int_arg_num = 1
     str_uname = str_input.lstrip("ssh://")[0:6]
+    str_uname = str_uname.split(";")[0]
     str_password = str_input.lstrip("ssh://"+str_uname+";password=").split("@ictencsvr")[0]
+    str_password = str_password.lstrip("%3Bpassword%3D")
     str_server = str_input.split("@")[1].strip("/")
     str_locale = str_uname+"@"+str_server
 
@@ -25,22 +28,23 @@ if __name__ == "__main__":
     print(url_host("$1"))
 END
 }
-if locate "/bin/python" && locate "/bin/sshpass"
+
+if (locate "/bin/python" && locate "/bin/sshpass") || (clear && echo "Please install Python and sshpass")
 then
+    echo "URL Collected"
+    str_url_pass=$(call_python "$1") || (clear && echo " " && echo "Please Give a ssh URL as first argument encapsulated with quotes" && exit 1);
 
-str_url_pass=$(call_python "$1");
+    IFS=', ' read -ra arr_result_bash <<<$str_url_pass; 
 
-IFS=', ' read -ra arr_result_bash <<<$str_url_pass; 
+    echo "Host = "${arr_result_bash[0]};
+    echo "Password = "${arr_result_bash[1]};
 
-echo "Host = "${arr_result_bash[0]};
-echo "Password = "${arr_result_bash[1]};
+    sshpass -p ${arr_result_bash[1]} ssh -o StrictHostKeyChecking=no ${arr_result_bash[0]}
 
-sshpass -p ${arr_result_bash[1]} ssh -o StrictHostKeyChecking=no ${arr_result_bash[0]}
-
-read emp
+    read emp
 
 else
 
     echo "Please make sure sshpass and python are installed before running this script."
-
+   
 fi
